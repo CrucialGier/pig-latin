139f6a07fe9b03ea6e6084eec3defd8db51b4dd1
shatha shaath and Stewart Cole <shatha@>
2016-06-08 16:26:02 -0700

    takes in any sentence and translates it into pigLatin

--- js/scripts.js
+++ js/scripts.js
@@ -2,21 +2,21 @@
 $(document).ready(function() {
   $("#go").click(function() {
     debugger;
-    var word = $("#sentence").val();
+    var input = $("#sentence").val().split(" ");
     var vowels = ['a' , 'e', 'i', 'o', 'u', 'y'];
-
-    if (word.charAt(0) === "q" && word.charAt(1) === "u") {
+    var pigResult = input.map(function(word) {
+      if (word.charAt(0) === "q" && word.charAt(1) === "u") {
       word = word.replace("qu", "");
-      word = word + "quay";
+      return word + "quay";
     } else {
       for (var v = 0; v < vowels.length; v++) {
         if (word.charAt(0) === vowels[v]) {
-          word = word + "ay";
+          return word + "ay";
         } else {
           for (var i = 0; i < word.length; i++) {
             for (var j = 0; j < vowels.length; j++) {
               if (word.charAt(i) === vowels[j]) {
-                word = word.slice(i) + word.slice(0, i) + "ay";
+                return word.slice(i) + word.slice(0, i) + "ay";
                 break;
               };
               if (word.charAt(0) === vowels[j]) {
@@ -31,9 +31,11 @@ $(document).ready(function() {
         break;
       };
     };
+  });
+  alert(pigResult.join(" "));
 
 
-    alert(word);
+
 
 
   });
